a9ad4e393f9111243e4a8286385a6f7faa6b7b71
TJ <saravanan241196@gmail.com>
2021-05-24 15:44:18 +0530

    Fixied bug Add Thread Sleep to get delay for capturing Screenshots,

--- src/main/java/com/SnapShotApp/CounterTest.java
+++ src/main/java/com/SnapShotApp/CounterTest.java
@@ -27,19 +27,22 @@ public class CounterTest extends JFrame implements ActionListener {
 	private JButton takeSnapeButtonVeriable;
 	private JButton choosePathVeriable;
 	private static int a;
-	
+
 	String isPath = null;
 	boolean isfilePathCounterCreater;
-	
+
 	private int count;
 	static String countValues;
 
 	public static final String ERRORMSG = "Did you not select the path?";
 	public static final String TITLE = "SnapShot-0.2v";
 	public static final String WINDOWSUSERNAME = System.getProperty("user.name");
+	
+	final JFrame jfrm= new JFrame(TITLE);
+
 
 	public CounterTest() {
-		setTitle(TITLE);
+		//setTitle(TITLE);
 		count = 0;
 
 		choosePathVeriable = new JButton("Path");
@@ -55,95 +58,97 @@ public class CounterTest extends JFrame implements ActionListener {
 
 		developerNameLabel = new JLabel("Developed By Saravanakumar Tj");
 		developerNameLabel.setBounds(20, 110, 300, 30);
-		add(choosePathVeriable);
-		add(filePathLabel);
-		add(takeSnapeButtonVeriable);
-		//add(developerNameLabel);
-		setSize(256, 110);
-		setLocationRelativeTo(null);
-		setLayout(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		setResizable(true);
+		jfrm.add(choosePathVeriable);
+		jfrm.add(filePathLabel);
+		jfrm.add(takeSnapeButtonVeriable);
+		// add(developerNameLabel);
+		jfrm.setSize(256, 110);
+		jfrm.setLocationRelativeTo(null);
+		jfrm.setLayout(null);
+		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jfrm.setVisible(true);
+		jfrm.setResizable(true);
 	}
 
 	public void actionPerformed(ActionEvent ae) {
-		JFileChooser fileChooser = new JFileChooser();
-		File file = null;
+		synchronized (ae) {
 
-		if (ae.getSource() == choosePathVeriable) {
+			JFileChooser fileChooser = new JFileChooser();
+			File file = null;
 
-			fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-			int option = fileChooser.showOpenDialog(CounterTest.this);
-			if (option == JFileChooser.APPROVE_OPTION) {
-				file = fileChooser.getSelectedFile();
-				filePathLabel.setText(file.getAbsolutePath());
-				isPath = file.getAbsolutePath();
+			if (ae.getSource() == choosePathVeriable) {
 
-			} else {
-				// filePathLabel.setText(ERRORMSG);
-			}
-
-		}
-
-		if (null != isPath && ae.getSource() == takeSnapeButtonVeriable) {
-			
-			count++;
-			countValues = String.valueOf(count);
-			
-			setVisible(false);
-			try {
-				Robot r = new Robot();
-
-
-				String pathofCounter = "C:\\"+"Users\\"+WINDOWSUSERNAME+"\\"+"SnapShotAppCache"+"\\";
-				String path = filePathLabel.getText();
-				new File(pathofCounter).mkdir();
-				if (new File(pathofCounter + "cache.txt").createNewFile()) {
-					FileWriter myWriter = new FileWriter(pathofCounter + "cache.txt");
-					myWriter.write("1");
-					myWriter.close();
-
-					
-			
+				fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int option = fileChooser.showOpenDialog(CounterTest.this);
+				if (option == JFileChooser.APPROVE_OPTION) {
+					file = fileChooser.getSelectedFile();
+					filePathLabel.setText(file.getAbsolutePath());
+					isPath = file.getAbsolutePath();
 
 				} else {
-
-					File file1 = new File("C:\\"+"Users\\"+WINDOWSUSERNAME+"\\"+"SnapShotAppCache"+"\\"+"cache.txt");
-					FileInputStream fin = new FileInputStream(file1);
-					byte fileContent[] = new byte[(int) file1.length()];
-					fin.read(fileContent);
-					String s = new String(fileContent);
-					a = Integer.parseInt(String.valueOf(s));
-					fin.close();
-
-					FileWriter myWriter = new FileWriter(pathofCounter + "cache.txt");
-					myWriter.write(String.valueOf(++a));
-					myWriter.close();
-
+					// filePathLabel.setText(ERRORMSG);
 				}
 
-				File file2 = new File("C:\\"+"Users\\"+WINDOWSUSERNAME+"\\"+"SnapShotAppCache"+"\\"+"cache.txt");
-				FileInputStream fin1 = new FileInputStream(file2);
-				byte fileContent1[] = new byte[(int) file2.length()];
-				fin1.read(fileContent1);
-				String countValues1 = new String(fileContent1);
-				fin1.close();
-
-				Rectangle capture = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-				BufferedImage Image = r.createScreenCapture(capture);
-				ImageIO.write(Image, "jpg", new File(path + "\\" + "" + countValues1 + ".jpg"));
-
-			} catch (Exception ex) {
-				System.out.println(ex.getMessage());
 			}
 
-		}
-		if (null == isPath) {
-			JOptionPane.showMessageDialog(new JFrame(), ERRORMSG, TITLE, JOptionPane.ERROR_MESSAGE);
-		}
+			if (null != isPath && ae.getSource() == takeSnapeButtonVeriable) {
 
-		setVisible(true);
+				count++;
+				countValues = String.valueOf(count);
+
+				try {
+					Robot r = new Robot();
+
+					String pathofCounter = "C:\\" + "Users\\" + WINDOWSUSERNAME + "\\" + "SnapShotAppCache" + "\\";
+					String path = filePathLabel.getText();
+					new File(pathofCounter).mkdir();
+					if (new File(pathofCounter + "cache.txt").createNewFile()) {
+						FileWriter myWriter = new FileWriter(pathofCounter + "cache.txt");
+						myWriter.write("1");
+						myWriter.close();
+
+					} else {
+
+						File file1 = new File(
+								"C:\\" + "Users\\" + WINDOWSUSERNAME + "\\" + "SnapShotAppCache" + "\\" + "cache.txt");
+						FileInputStream fin = new FileInputStream(file1);
+						byte fileContent[] = new byte[(int) file1.length()];
+						fin.read(fileContent);
+						String s = new String(fileContent);
+						a = Integer.parseInt(String.valueOf(s));
+						fin.close();
+
+						FileWriter myWriter = new FileWriter(pathofCounter + "cache.txt");
+						myWriter.write(String.valueOf(++a));
+						myWriter.close();
+
+					}
+
+					File file2 = new File(
+							"C:\\" + "Users\\" + WINDOWSUSERNAME + "\\" + "SnapShotAppCache" + "\\" + "cache.txt");
+					FileInputStream fin1 = new FileInputStream(file2);
+					byte fileContent1[] = new byte[(int) file2.length()];
+					fin1.read(fileContent1);
+					String countValues1 = new String(fileContent1);
+					fin1.close();
+					jfrm.setVisible(false);
+					try {Thread.sleep(150);} catch (InterruptedException ie) {Thread.currentThread().interrupt();}
+					Rectangle capture = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+					BufferedImage Image = r.createScreenCapture(capture);
+					ImageIO.write(Image, "jpg", new File(path + "\\" + "" + countValues1 + ".jpg"));
+					jfrm.setVisible(true);
+
+				} catch (Exception ex) {
+					System.out.println(ex.getMessage());
+				}
+
+			}
+			if (null == isPath) {
+				JOptionPane.showMessageDialog(new JFrame(), ERRORMSG, TITLE, JOptionPane.ERROR_MESSAGE);
+			}
+
+			
+		}
 	}
 
 	public static void main(String[] args) {
